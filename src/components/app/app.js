35771981073740b6e59b5b8c762cc8b1ce088632
i_ivashkevich <i_ivashkevich@wargaming.net>
2022-12-19 16:01:35 +0200

    public

---
+++ src/components/app/app.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import Request from '../../service/request';
+
+import './app.css';
+
+const request = new Request();
+
+export default class App extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (element) => {
+        this.setState({
+            label: element.target.value
+        });
+    };
+
+    onSubmit = async (element) => {
+        element.preventDefault();
+        console.log('request: ' + this.state.label);
+        if (this.state.label !== '') {
+            await request.getResource(this.state.label)
+        }
+    };
+
+    render() {
+        return (
+            <div className="main">
+                <form onSubmit={this.onSubmit}>
+                    <input
+                        className="form"
+                        type="text"
+                        placeholder="Введите запрос"
+                        onChange={this.onLabelChange}
+                        value={this.state.label}
+                    />
+                    <button
+                        className="btn-secondary button-custom"
+                        onClick={this.onSubmit}>
+                        Send
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
